755188807d36da1f05db4f467b8e1b934e2a25c3
Sicnecher <efraty665@gmail.com>
2025-05-17 02:26:29 +0300

    feat: final

--- client/src/services/socket.service.ts
+++ client/src/services/socket.service.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("http://localhost:3000")
+export const socket = io("/")
 
 export function handleEmitter(socketName: string, song?: any){
     socket.emit(socketName, song)
